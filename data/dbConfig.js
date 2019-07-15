bc19c659ed759cb0405f6136350ebd82c6122046
ropeks <ropeksmilioner@gmail.com>
2019-07-15 15:31:07 +0200

    add GET endpoints for accounts resource

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,24 @@
 const knex = require('knex');
 
+const db = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: './data/budget.db3',
+    },
+    useNullAsDefault: true,
+});
+
+function getAccounts() {
+    return db('accounts');
+}
+
+function getAccountById(id) {
+    return db('accounts').where({ id });
+}
+
 const configOptions = require('../knexfile').development;
 
-module.exports = knex(configOptions);
+module.exports = {
+    getAccounts,
+    getAccountById
+};
--- server.js
+++ server.js
@@ -6,4 +6,18 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.json('this is my first db');
+});
+
+server.get('/accounts', async (req, res) => {
+    const accounts = await db.getAccounts();
+    res.json(accounts);
+});
+
+server.get('/accounts/:id', async (req, res) => {
+    const accounts = await db.getAccountById(req.params.id);
+    res.json(accounts[0]);
+})
+
 module.exports = server;
